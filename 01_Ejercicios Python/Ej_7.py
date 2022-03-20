13146e773fc587c14d02b14f5bbb0c36622b0e7e
Bautista Cimatti <bcimatti2005@gmail.com>
2022-03-20 12:51:04 -0300

    Ejercicio 7 agregado

---
+++ 01_Ejercicios Python/Ej_7.py
@@ -0,0 +1,15 @@
+def isStep(num):
+    if int(len(num)%2!=0):
+        return False
+
+    for i in range(0,len(num),2):
+        if not (int(num[i:i+2][0])+1==int(num[i:i+2][1]) or int(num[i:i+2][0])-1==int(num[i:i+2][1])):
+            return False
+    return True
+
+nmbr=input("Ingrese un numero para averiguar si es STEP: ")
+
+if (isStep(nmbr)):
+    print("Es numero STEP.")
+elif(isStep(nmbr)==False):
+    print("No es numero STEP.")
