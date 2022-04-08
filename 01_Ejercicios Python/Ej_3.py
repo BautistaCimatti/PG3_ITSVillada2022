e1177c0fe1a180c7a00957036171a07b0f44182b
Bautista Cimatti <bcimatti2005@gmail.com>
2022-04-08 11:58:54 -0300

    Correccion desafio 1(Ej_3)

--- 01_Ejercicios Python/Ej_3.py
+++ 01_Ejercicios Python/Ej_3.py
@@ -1,13 +1,11 @@
-largo = int(input("Ingrese el largo: "))
-char = input("Ingrese el caracter a utilizar: ")
-
-def draw_triangle(largo:int, char:str)->None:
+def draw_triangle()->None:
+    largo = int(input("Ingrese el largo: "))
+    char = input("Ingrese el caracter a utilizar: ")
     cont:int=1
     
     for x in range(largo):
         spaces:int=largo-cont
         print((" ") * spaces + (char + " ") * cont)
         cont+=1
-        
 
-draw_triangle(largo,char)
+draw_triangle()
