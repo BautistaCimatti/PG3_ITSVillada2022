cc0bfe55e54398df196030bf8a15ab439ffbaf5b
Bautista Cimatti <bcimatti2005@gmail.com>
2022-04-08 11:57:09 -0300

    Correccion Ej_3

--- 01_Ejercicios Python/Ej_3.py
+++ 01_Ejercicios Python/Ej_3.py
@@ -1,6 +1,9 @@
-ancho = int(input("Ingrese el ancho: "))
-largo = int(input("Ingrese el largo: "))
-char = input("Ingrese el caracter a utilizar: ")
 
-for x in range(largo):
-    print((char + " ") * ancho)
+def draw_square():
+    ancho = int(input("Ingrese el ancho: "))
+    largo = int(input("Ingrese el largo: "))
+    char = input("Ingrese el caracter a utilizar: ")
+    for x in range(largo):
+        print((char + " ") * ancho)
+
+draw_square()        
