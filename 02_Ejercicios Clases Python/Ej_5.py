ccc311cfe620d09a15e6a63b765c6d7b88472261
Bautista Cimatti <bcimatti2005@gmail.com>
2022-04-25 11:31:38 -0300

    Ej_5

---
+++ 02_Ejercicios Clases Python/Ej_5.py
@@ -0,0 +1,32 @@
+class Persona:
+
+    def __init__(self):
+        self.nombre=input("Nombre:")
+        self.edad=int(input("Edad:"))
+
+    def imprimir(self):
+        print("Nombre:",self.nombre)
+        print("Edad:",self.edad)
+
+
+class Empleado(Persona):
+
+    def __init__(self):
+        super().__init__()
+        self.sueldo=int(input("Sueldo:"))
+
+    def imprimir(self):
+        super().imprimir()
+        print("Sueldo:",self.sueldo)
+
+    def impuestos(self):
+        if self.sueldo>3000:
+            print("Paga impuestos")
+        else:
+            print("No paga impuestos")
+
+p1=Persona()
+p1.imprimir()
+e1=Empleado()
+e1.imprimir()
+e1.impuestos()
