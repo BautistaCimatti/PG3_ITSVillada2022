851ed0031ac43dde6d39d6f346e2142f65c8d521
Bautista Cimatti <bcimatti2005@gmail.com>
2022-04-25 11:46:15 -0300

    Ej_6

---
+++ 02_Ejercicios Clases Python/Ej_6.py
@@ -0,0 +1,14 @@
+class Familia:
+    def __init__(self):
+        self.padre = str(input("Nombre del padre: "))
+        self.madre = str(input("Nombre de la Madre: "))
+        self.cont = int(input("Cantidad de hijos: "))
+        self.hijos = []
+        for i in range(self.cont):
+            self.hijos.append(str(input("Nombre del hijo: ")))
+        
+    def __str__(self)->str:
+        return f"Padre: {self.padre} \nMadre: {self.madre} \nHijos:  {self.hijos}"
+
+f1=Familia()
+print(f1)
