0928ec58b28618ce9962177ba41b40730d93d2b0
Bautista Cimatti <bcimatti2005@gmail.com>
2022-05-05 09:05:28 -0300

    Ej_05

---
+++ 03_Ejercicios Manejo de excepciones Python/Ej_05.py
@@ -0,0 +1,19 @@
+try:
+    f = open("demofile.txt", "w")
+    i= int(input("Ingresa lo que quieras guardar en el txt: "))
+    if i.isnumeric()==True:
+        int(i)
+    else:
+        str(i)
+
+    f.write(i)
+    f.close()
+
+    f = open("demofile.txt", "r")
+    print(f.read()) 
+
+except TypeError:
+        f.write(str(i))
+        f.close()
+        f = open("demofile.txt", "r")
+        print(f.read()) 
